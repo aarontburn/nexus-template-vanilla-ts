27e9713d48e8e257c4f59c1954ed39ec9e9f6f4b
Aaron <aarontburnham@hotmail.com>
2024-06-05 22:44:36 -0700

    Updated template

--- src/built_ins/home_module/HomeRenderer.ts
+++ src/built_ins/home_module/HomeRenderer.ts
@@ -9,12 +9,17 @@
 
     sendToProcess("init");
 
+    const displayContainer: HTMLElement = document.getElementById('center');
+
     const fullDate: HTMLElement = document.getElementById("full-date");
     const abbreviatedDate: HTMLElement = document.getElementById("abbreviated-date");
     
     const standardTime: HTMLElement = document.getElementById("standard-time");
     const militaryTime: HTMLElement = document.getElementById("military-time");
     
+    let currentOrder: string = undefined 
+
+
     window.parent.ipc.on(MODULE_RENDERER_NAME, (_, eventType: string, ...data: any[]) => {
         switch(eventType) {
             case "update-clock": {
@@ -29,12 +34,67 @@
                 abbreviatedDate.style.fontSize = data[0].abbrDate + "px";
                 standardTime.style.fontSize = data[0].standardTime + "px";
                 militaryTime.style.fontSize = data[0].militaryTime + "px";
+                break;
+            }
+            case 'display-order': {
+                const order: string = data[0];
 
-
+                if (currentOrder === undefined || currentOrder !== order) {
+                    changeDisplayOrder(order);
+                }
                 break;
             }
         }
     });
+
+
+    function changeDisplayOrder(newOrder: string): void {
+        currentOrder = newOrder;
+
+        while (displayContainer.firstChild) {
+            displayContainer.removeChild(displayContainer.lastChild);
+        }
+
+
+        for (const char of newOrder.split('')) {
+            switch (char) {
+                case '1': {
+                    displayContainer.insertAdjacentElement("beforeend", fullDate);
+                    break;
+                }
+                case '2': {
+                    displayContainer.insertAdjacentElement("beforeend", abbreviatedDate);
+                    break;
+                }
+                case '3': {
+                    displayContainer.insertAdjacentElement("beforeend", standardTime);
+                    break;
+                }
+
+                case '4': {
+                    displayContainer.insertAdjacentElement("beforeend", militaryTime);
+                    break;
+                }
+                case ' ': {
+                    displayContainer.insertAdjacentHTML("beforeend", "<br />");
+                    break;
+                }
+                default: {
+                    // changeDisplayOrder('12 34');
+                    throw new Error("Invalid order: " + newOrder);
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
 })();
 
 
