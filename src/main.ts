39e785a8ad1fa452be11f350bb226fa9bcb9669b
Aaron <aarontburnham@hotmail.com>
2025-03-30 23:20:08 -0700

    cleaned

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { app } from "electron";
+import { app as _ } from "electron";
 
-
-
-
+// This file is to indicate that Electron is being used and SHOULD NOT be modified. 
+// Can be safely ignored. Do not add logic here.
