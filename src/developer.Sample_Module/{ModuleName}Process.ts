c5770ffde073f336dcf308cf16a35fa59220540b
Aaron <aarontburnham@hotmail.com>
2025-04-01 00:41:57 -0700

    swapped to default

--- src/developer.Sample_Module/{ModuleName}Process.ts
+++ src/developer.Sample_Module/{ModuleName}Process.ts
@@ -5,7 +5,7 @@ import { BooleanSetting, ChoiceSetting, HexColorSetting, NumberSetting, StringSe
 
 
 
-export class SampleModuleProcess extends Process {
+export default class SampleModuleProcess extends Process {
 
     private static readonly MODULE_NAME: string = "Sample Module";
     private static readonly MODULE_ID: string = "developer.Sample_Module";
